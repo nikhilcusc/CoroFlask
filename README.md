81769688ef27e20f1da633efdacf4cc38848a447
nikhilcusc <nikhilc@usc.edu>
2020-04-17 23:08:17 -0700

    added back button and fixed countyName list problem

--- README.md
+++ README.md
@@ -7,10 +7,11 @@
 ![Screen Shot 16-04-2020](/Screenshots/ScreenShot2020-04-16181131.jpg)
 
 ## TODO:
-	- [x] Get both the charts displaying.
-	- [x] Update 'updateChart' function to toggle between daily, weekly, biweekly avergaes. 
-	- [ ] Update to select any county from a drop down.
-		- [ ] Implement drop down over a text box. 
-	- [ ] Fix reading CountyName from a list.
-	- [ ] Add validation checks on County Name.
+- [x] Get both the charts displaying.
+- [x] Update 'updateChart' function to toggle between daily, weekly, biweekly avergaes.
+- [x] Add a back button. 
+- [ ] Update to select any county from a drop down.
+	- [ ] Implement drop down over a text box. 
+- [x] Fix reading CountyName from a list.
+- [ ] Add validation checks on County Name.
 
--- Sol/websw.py
+++ Sol/websw.py
@@ -13,19 +13,20 @@ def home():
 @app.route('/Corochart', methods=['POST'])
 def Corochart():
 	#get county name
-	CountyName = [request.form['county']]
-	#CountyName = ['Los Angeles'];	
+	CountyName = request.form['county']
+	#validate the CountyName here
 	#get the rawdata from website
 	response = urllib2.urlopen(rawURL)
 	cr = csv.reader(response)
 	countyData=[]
 	for row in cr:
 		#row date,county,state,fips,cases,deaths
-		if row[1]==CountyName[0]:
+		if row[1]==CountyName:
 			countyData.append(row)
 	
 	dates=[row[0] for row in countyData]
 	cases=[row[4] for row in countyData]
 	deaths=[row[5] for row in countyData]
-
-	return render_template('Corochart.html',title='Corochart', CountyName=CountyName, dates=dates, cases=cases, deaths=deaths)
+	row = countyData[0]
+	county = [row[1],row[2]]
+	return render_template('Corochart.html',title='Corochart', countyData=county, dates=dates, cases=cases, deaths=deaths)
