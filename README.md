b1e745c841362f707ee96454d373fecb9cfdf37a
nikhilcusc <nikhilc@usc.edu>
2020-05-31 14:56:30 -0700

    now list of states are pulled from database

--- README.md
+++ README.md
@@ -20,11 +20,11 @@ I have also changed the way graphs display data. I wanted to see total number of
 	- [x] Add 3-day period for completeness.
 	- [ ] Take averages instead of showing just the dates's values.
 - [x] Add a back button. 
-- [ ] Update to select any county from a drop down. (new branch)
+- [x] Update to select any county from a drop down. (new branch)
 	- [x] Implement drop down over a text box.
 		- [x] Get county names based on state selected.
 		- [x] Use this county name to display the charts.
-		- [ ] Include all US states. Currently, Arizona and California are only selectable.
+		- [x] Include all US states. ~Currently, Arizona and California are only selectable.~
 	- [ ] Extend this to include other countries, if possible!
 - [x] Fix reading CountyName from a list.
 - [x] Add empty validation check on entered County Name.
--- Sol/websw.py
+++ Sol/websw.py
@@ -19,6 +19,25 @@ app.secret_key = 'EcCbAbg7nZ' #haha not so secret!
 class Validation(Form):
     county = StringField('county', [validators.DataRequired()])
 
+@app.route('/_getStates')
+def getStates():
+	stateSelected = request.args.get('country')
+	
+	#get the rawdata from website
+	response = urllib2.urlopen(rawURL)
+	cr = csv.reader(response)
+	
+	statesList= []
+	for row in cr:
+		#print(row)
+		try:
+			if row[2] not  in statesList :
+				statesList.append(row[2])
+		except IndexError:
+			continue
+	statesList = statesList[1:] #remove header "states" from the list
+	return jsonify({"statesListNames":statesList})
+
 @app.route('/_getCounties')
 def getCounties():
 	stateSelected = request.args.get('state')
@@ -44,17 +63,6 @@ def getCounties():
 def home():
     return render_template('home.html')
 
-'''	
-@app.route('/Corochart', methods=['GET'])
-def Corochart():
-	#CountyNameDD = 'Maricopa'
-	#CountyNameDD = request.form.get('county')
-	CountyNameDD = request.args.get("counties")
-	CountyName = CountyNameDD 
-	CountyName.replace(",", "")
-	
-	return render_template('testDropDown.html',title='testDropDown', countyData=CountyNameDD)
-'''
 
 @app.route('/Corochart', methods=['GET'])
 def Corochart():
@@ -100,6 +108,6 @@ def Corochart():
 		stackedDeaths.append( int(countyData[ind][5]) - int(countyData[ind-1][5]))
 		trailingSumStackedDeaths.append(int(countyData[ind-1][5]))
 
-
 	county = [row[1],row[2]]
+	
 	return render_template('Corochart.html',title='Corochart', countyData=county, dates=dates, cases=cases, stackedCases=stackedCases,  trailingSumStackedCases=trailingSumStackedCases, deaths=deaths, stackedDeaths=stackedDeaths, trailingSumStackedDeaths=trailingSumStackedDeaths)
